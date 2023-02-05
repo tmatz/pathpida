74c59112af284be1df240f3c68b5182ff5f63124
MATSUDA Takashi <tmatz.dev@gmail.com>
2023-02-05 22:33:24 +0900

    alias name based on import path for stable diff

--- src/parseQueryFromTS.ts
+++ src/parseQueryFromTS.ts
@@ -9,13 +9,22 @@ export const parseQueryFromTS = (output: string, file: string, suffix: number) =
 
   if (!typeName) return
 
-  const importName = `${typeName}${suffix}`
+  const importPath = path
+    .relative(output, file)
+    .replace(/\\/g, '/')
+    .replace(/(\/index)?\.tsx?$/, '')
+
+  const importName = `${typeName}${makeSuffix(suffix, importPath)}`
 
   return {
     importName,
-    importString: `import type { ${typeName} as ${importName} } from '${path
-      .relative(output, file)
-      .replace(/\\/g, '/')
-      .replace(/(\/index)?\.tsx?$/, '')}'`
+    importString: `import type { ${typeName} as ${importName} } from '${importPath}'`
   }
+}
+
+function makeSuffix(suffix: number, importPath: string): string {
+  if (suffix !== -1) {
+    return `${suffix}`
+  }
+  return `_${importPath.replace(/[^A-Za-z0-9_$]/g, c => (c === '/' ? '$' : '_'))}`
 }
